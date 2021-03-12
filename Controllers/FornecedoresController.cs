3f0eff424b5ccf6fc5e5604c69e94239a8cd6aeb
Daniel Morais Borges <daniel.morais@bumlai.com.br>
2021-03-12 19:34:42 -0400

    Criação Rota api/home para trás mensagem de API FUNCIONANDO

--- Controllers/FornecedoresController.cs
+++ Controllers/FornecedoresController.cs
@@ -2,8 +2,6 @@
 using WebApiFornecedor.Models;
 using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace WebApiFornecedor.Controllers
 {
     [Route("api/[controller]")]
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace WebApiFornecedor.Controllers
+{
+    [Route("api/")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        [Route("home")]
+        public string Home() => " API Fornecedores Iniciada . . . ";
+
+    }
+}
